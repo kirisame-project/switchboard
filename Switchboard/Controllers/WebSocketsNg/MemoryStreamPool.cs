23c57d113f2d488cb1b08701e903ba71951c4392
amphineko <sukareki@gmail.com>
2019-11-08 12:31:39 +0800

    add memorystreampool reference check before renting

--- Switchboard/Controllers/WebSocketsNg/MemoryStreamPool.cs
+++ Switchboard/Controllers/WebSocketsNg/MemoryStreamPool.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using Switchboard.Common;
 
 namespace Switchboard.Controllers.WebSocketsNg
@@ -10,7 +11,12 @@ namespace Switchboard.Controllers.WebSocketsNg
     public class MemoryStreamPool : IObjectOwner<MemoryStream>
     {
         private readonly int? _maximumRetained;
-        private readonly ConcurrentBag<WeakReference<MemoryStream>> _pool;
+
+        private readonly IProducerConsumerCollection<WeakReference<MemoryStream>> _pool;
+
+        private long _referenceMissCount;
+
+        private long _streamClosedCount;
 
         public MemoryStreamPool(int? maximumRetained = null)
         {
@@ -21,18 +27,26 @@ namespace Switchboard.Controllers.WebSocketsNg
         public void Return(MemoryStream stream)
         {
             if (_maximumRetained != null && _maximumRetained < _pool.Count) return;
-
-            stream.SetLength(0);
-            _pool.Add(new WeakReference<MemoryStream>(stream));
-
-            Debug.Assert(stream.Position == 0);
+            var result = _pool.TryAdd(new WeakReference<MemoryStream>(stream));
+            Debug.Assert(result);
         }
 
         public MemoryStream Get()
         {
             while (_pool.TryTake(out var reference))
                 if (reference.TryGetTarget(out var stream))
-                    return stream;
+                    if (stream.CanRead && stream.CanWrite)
+                    {
+                        stream.SetLength(0);
+                        return stream;
+                    }
+                    else
+                    {
+                        Interlocked.Increment(ref _streamClosedCount);
+                    }
+                else
+                    Interlocked.Increment(ref _referenceMissCount);
+
             return new MemoryStream();
         }
     }
